9fc16333ec422524b50493eca240baca8c046250
qinwl <qinwl@163.com>
2019-01-10 18:31:01 +0800

    ...

--- cookie/index.go
+++ cookie/index.go
@@ -9,6 +9,7 @@ func init() {
 	http.HandleFunc("/setCookie", setCookie)
 	http.HandleFunc("/getCookie", getCookie)
 	http.HandleFunc("/delCookie", delCookie)
+	http.HandleFunc("/addCookie", addCookie)
 }
 
 // 设置cookie
@@ -16,9 +17,27 @@ func setCookie(w http.ResponseWriter, r *http.Request) {
 	if r.RequestURI == "/favicon.ico" {
 		return
 	}
-	cookie := http.Cookie{Name: "IamCookie", Value: "asd123", Path: "/", MaxAge: 86400}
+	cookie := http.Cookie{
+		Name:   "IamCookie",
+		Value:  "asd123",
+		Path:   "/",
+		MaxAge: 86400,
+	}
 	http.SetCookie(w, &cookie)
 	fmt.Fprintf(w,"HELLO, I AM Cookie")
+}
+
+// 增加cookie，使用w.Header().Add("Set-Cookie", cookie对象)
+func addCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name: "first_cookie",
+		Value: "vanyar",
+		HttpOnly: true,
+	}
+	if r.RequestURI == "/favicon.ico" {
+		return
+	}
+	w.Header().Add("Set-Cookie",c1.String())
 }
 
 // 获取cookie
@@ -26,13 +45,24 @@ func getCookie(w http.ResponseWriter, r *http.Request) {
 	if r.RequestURI == "/favicon.ico" {
 		return
 	}
+	// 根据key来获取某个cookie
 	cookie, err := r.Cookie("IamCookie")
 	if err != nil {
 		fmt.Println(err)
-		return
+		fmt.Println("IamCookie不存在")
 	}
-	fmt.Println(cookie)
+	fmt.Println(" r.Cookie: ", cookie)
+
+	// 使用r.Header["Cookie"]获取所有的cookie，返回一个数组
+	h := r.Header["Cookie"]
+	fmt.Println("r.Header['Cookie']: ", h)
+
+	// 使用 r.Cookies() 获取所有的cookie
+	c2 := r.Cookies()
+	fmt.Println("r.Cookies(): ", c2)
+
 	fmt.Fprintf(w,"HELLO, I AM 获取cookie")
+
 }
 
 // 删除cookie
@@ -40,7 +70,12 @@ func delCookie(w http.ResponseWriter, r *http.Request) {
 	if r.RequestURI == "/favicon.ico" {
 		return
 	}
-	cookie := http.Cookie{Name: "IamCookie", Path: "/", MaxAge: -1}
+
+	cookie := http.Cookie{
+		Name:   "IamCookie",
+		Path:   "/",
+		MaxAge: -1,
+	}
 	http.SetCookie(w, &cookie)
 	fmt.Fprintf(w,"HELLO, I AM delCookie")
 }
